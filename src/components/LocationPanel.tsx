4e627d6e823be50fddb088ee51c82f536536992d
Jennifer Adams <jennadamscodes@gmail.com>
2025-01-24 17:35:44 -0500

    Create LocationPanel component to display time and current weather

---
+++ src/components/LocationPanel.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { WeatherApiClient, WeatherApiResponse } from "~services/WeatherApiClient";
+
+type LocationPanelProps = {
+    apiKey: string;
+    locationQuery: string;
+}
+
+const LocationPanel = (props: LocationPanelProps) => {
+    const weatherApiClient = new WeatherApiClient(props.apiKey);
+    const [currentData, setCurrentData] = useState<WeatherApiResponse>();
+
+    useEffect(() => {
+        async function getCurrentData() {
+            const data = await weatherApiClient.getCurrent(props.locationQuery);
+            setCurrentData(data);
+        }
+        getCurrentData().then();
+    }, []);
+
+    return (
+        <div className='location-panel'>
+            <h2>{currentData?.locationName}</h2>
+            <p>{currentData?.dateTime}</p>
+            <p>{currentData?.temperature}</p>
+            <p>{currentData?.condition}</p>
+        </div>
+    );
+}
+
+export default LocationPanel;
