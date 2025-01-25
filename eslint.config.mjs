7df487626d2f0ed84b9df0aadf08507304e6599f
Jennifer Adams <jennadamscodes@gmail.com>
2025-01-24 22:17:41 -0500

    Install eslint and configure rules

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+const noUnusedConfig = [
+  "warn",
+  {
+    "argsIgnorePattern": "^_",
+    "varsIgnorePattern": "^_",
+    "caughtErrorsIgnorePattern": "^_",
+  },
+];
+
+const customRules = [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      "react": {
+        "version": "detect",
+      },
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs,
+    },
+    rules: {
+      "no-unused-vars": noUnusedConfig,
+      "no-undef": "warn",
+      "@typescript-eslint/no-unused-vars": noUnusedConfig,
+      "@stylistic/js/jsx-quotes": [ "error", "prefer-double" ],
+      "@stylistic/js/semi": [ "error", "always" ],
+      "@stylistic/js/comma-dangle": [ "error", "always-multiline"],
+    },
+  },
+];
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  ...customRules,
+];
