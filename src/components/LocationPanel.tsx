99b8aa6b6280e9a4626b05a4c891657e1d8726ee
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-08 01:15:10 -0500

    Refactor MultiWeather component to handle getCurrent request using new api service and pass data to LocationPanel

--- src/components/LocationPanel.tsx
+++ src/components/LocationPanel.tsx
@@ -1,31 +1,21 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { WeatherApiClient, WeatherApiResponse } from "~services/WeatherApiClient";
 
-type LocationPanelProps = {
-    apiKey: string;
-    locationQuery: string;
+export type LocationPanelProps = {
+    locationName: string;
+    dateTime: string;
+    temperature: string;
+    conditionText: string;
+    conditionIcon: string;
 }
 
 const LocationPanel = (props: LocationPanelProps) => {
-    const weatherApiClient = new WeatherApiClient(props.apiKey);
-    const [currentData, setCurrentData] = useState<WeatherApiResponse>();
-
-    useEffect(() => {
-        async function getCurrentData() {
-            const data = await weatherApiClient.getCurrent(props.locationQuery);
-            setCurrentData(data);
-        }
-        getCurrentData().then();
-    }, []);
-
     return (
         <div className="location-panel">
-            <img className="condition-icon" src={currentData?.conditionIcon} />
-            <h2 className="location-name">{currentData?.locationName}</h2>
-            <p>{currentData?.dateTime}</p>
-            <p>{currentData?.temperature}</p>
-            <p>{currentData?.conditionText}</p>
+            <img className="condition-icon" src={props.conditionIcon} />
+            <h2 className="location-name">{props.locationName}</h2>
+            <p>{props.dateTime}</p>
+            <p>{props.temperature}</p>
+            <p>{props.conditionText}</p>
         </div>
     );
 };
--- src/components/MultiWeather.tsx
+++ src/components/MultiWeather.tsx
@@ -1,26 +1,58 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 import LocationPanel from "./LocationPanel";
+import { ApiService, Current } from "~services/ApiService";
 
 const MultiWeather = () => {
-    const [apiKey, setApiKey] = useState<string>("");
-    const [locations, setLocations] = useState<string[]>([]);
+    const apiService = new ApiService();
+    const [ locations, setLocations ] = useState<Current[]>([]);
 
     useEffect(() => {
+        async function getCurrentData() {
+            const data = await apiService.getCurrent(locationNames);
+            setLocations(data);
+        }
+
         const queryString = location.search;
         const queryParams = new URLSearchParams(queryString);
-        const keyParam = queryParams.get("key") || "";
-        const qParam = queryParams.getAll("q") || [];
-        setApiKey(keyParam);
-        setLocations(qParam);
+        const locationNames = queryParams.getAll("q") || [];
+        getCurrentData().then();
     }, []);
+
+    const formatDateTimeString = (timeZone?: string): string => {
+        if (!timeZone)
+            return "";
+
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        const localDate = new Date();
+        const dateString = localDate.toLocaleDateString("en-US", { timeZone });
+        const [M,d] = dateString.split("/");
+        const timeString = localDate.toLocaleTimeString("en-US", { timeZone, timeZoneName:"short" });
+        const [h,m,_,ampm,tz] = timeString.split(/[: ]/);
+
+        return `${months[parseInt(M) - 1]} ${d}, ${h}:${m} ${ampm} (${tz})`;
+    };
+
+    const formatTempString = (tempC?: number, tempF?: number): string => {
+        if (!tempC || !tempF)
+            return "";
+        return `${tempC}° C | ${tempF}° F`;
+    };
 
     return (
         <div id="multi-weather">
             <h1>MultiWeather</h1>
-            {locations.map((locationQuery, index) => {
+            {locations.map((location, index) => {
                 const key = `LocationPanel${index}`;
-                return <LocationPanel key={key} apiKey={apiKey} locationQuery={locationQuery} />;
+                const locationPanelProps = {
+                    locationName: location.LocationName || "",
+                    dateTime: formatDateTimeString(location.TimeZone),
+                    temperature: formatTempString(location.TempC, location.TempF),
+                    conditionText: location.ConditionText || "",
+                    conditionIcon: location.ConditionIcon || "",
+                };
+                return <LocationPanel key={key} {...locationPanelProps}/>;
             })}
         </div>
     );
