fb7b3500bd19f6a93312349c4a73930aa6738bcf
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-08 01:07:43 -0500

    Add getCurrent request to api service

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -18,6 +18,26 @@ export class ApiService {
         
         return searchResults;
     }
+
+    public async getCurrent(locationNames: string[]) {
+        const queries = `q=${locationNames.join("&q=")}`;
+        const url = `${this.baseUrl}/multiweather/getcurrent?${queries}`;
+
+        const currentResults: Current[] = await fetch(url)
+            .then(async (jsonResponse) => {
+                const response = await jsonResponse.json();
+                if (!jsonResponse.ok)
+                    throw new Error(response.error.message);
+
+                return response;
+            })
+            .catch((error) => {
+                console.error("Error fetching current data:", error);
+            });
+
+        return currentResults;
+    }
+
 }
 
 export type SearchLocationResult = {
@@ -25,4 +45,15 @@ export type SearchLocationResult = {
     Longitude: string;
     ShortName: string;
     FullName: string;
+}
+
+export type Current = {
+    LocationQuery: string;
+    LocationFound: boolean;
+    LocationName?: string;
+    TimeZone?: string;
+    ConditionText?: string;
+    ConditionIcon?: string;
+    TempC?: number;
+    TempF?: number;
 }
