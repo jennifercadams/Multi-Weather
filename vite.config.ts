dd3cc519052dea973910c777fbce740fdb74c506
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-10 00:12:27 -0500

    Revert "Update vite base path"
    This reverts commit 79117c5842d76259671d40eb132c4b4394cd8e1a.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
-    base: '/',
+    base: '/Multi-Weather/',
     plugins:[
         tsconfigPaths(),
     ],
