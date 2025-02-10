fd49190d6c36c0a5fa9328e6d6536db6c7513b22
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-09 23:56:41 -0500

    Update base url for production

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,5 +1,5 @@
 export class ApiService {
-    private readonly baseUrl = "https://localhost:7211";
+    private readonly baseUrl = "https://distance-tools.onrender.com/";
 
     public async searchLocation(locationQuery: string) {
         const url = `${this.baseUrl}/multiweather/searchlocation?q=${locationQuery}`;
