9401264a2fd858b3246058dd2f68dd3d69aefa83
Jennifer Adams <jennadamscodes@gmail.com>
2025-02-27 09:59:14 -0500

    Create loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./LoadingSpinner.css";
+
+const LoadingSpinner = () => {
+    return ( <div className="loading-spinner" /> );
+};
+
+export default LoadingSpinner;
