7fe3d275f06ab103fa506617d58be89a20c7b4a8
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-24 16:35:31 -0400

    Add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import LocationSearch from "~pages/LocationSearch/LocationSearch";
 import MultiWeather from "~pages/MultiWeather/MultiWeather";
+import NotFoundPage from "~pages/NotFoundPage/NotFoundPage";
 import PageLayout from "~pages/PageLayout/PageLayout";
 
 const App = () => (
@@ -9,6 +10,8 @@ const App = () => (
         <Routes>
             <Route element={<PageLayout showActionButtons={false} />}> 
                 <Route index element={<LocationSearch />} />
+                <Route path="/404" element={<NotFoundPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Route>
             <Route element={<PageLayout showActionButtons={true} />}>
                 <Route path="current" element={<MultiWeather />} />
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./NotFoundPage.css";
+
+const NotFoundPage = () => {
+    return (
+        <div id="not-found">
+            <p className="small-text">PAGE NOT FOUND</p>
+            <p className="xl-text">404</p>
+            <p className="medium-text">Sorry! That page doesn&apos;t exist. Please check the url and try again. </p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
